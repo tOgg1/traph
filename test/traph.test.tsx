fb29a0a1117ffd920a34e39712df1a46b2e3a4df
Tormod Haugland <tormod.haugland@gmail.com>
2019-12-09 08:54:21 +0100

    Vastly improve traph
    This large commit touches most areas of how traph works internally. The
    changes facilitate the following functionality:
    	* Updating slices off the tree across multiple subtres.
    	* Attaching trees to multiple locations, and still get coherent
    state updates.
    	* Deleting subtrees.

--- test/traph.test.tsx
+++ test/traph.test.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import graph from '../src/traph'
+import React, { useContext } from 'react'
+import traph, { mergeGraphData } from '../src/traph'
 
-import { render, fireEvent, waitForElement } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { GraphType } from '../src/types'
 
 
-/**
- * Dummy test
- */
-describe("graph", () => {
+describe("graph basic usage", () => {
 
   it("Constructs a data graph successfully", () => {
-    graph({
+    traph({
       key: "value",
       user: {
         firstName: "Tormod",
@@ -21,19 +19,33 @@ describe("graph", () => {
   })
 
   it("Constructs a data graph with nested graphs successfully", () => {
-    graph({
+    traph({
       key: "value",
       user: {
         username: "Name"
       },
-      sub: graph({
+      sub: traph({
         whatIsThis: "Cool!"
       })
     })
   })
+
+  it("Accepts arrays as data", () => {
+    traph([2, 3, 4, traph({key: "value"})])
+  })
+
+  it("Accepts scalars as data", () => {
+    traph(2)
+    traph(2.45)
+    traph("Hello")
+    traph(undefined)
+    traph(null)
+  })
+})
   
+describe("graph hook usage", () => {
   it("makes a single data graph accessible using the useGraph hook", () => {
-    const dataGraph = graph({
+    const dataGraph = traph({
       user: {
         firstName: "Tormod",
         lastName: "Haugland" 
@@ -72,38 +84,8 @@ describe("graph", () => {
     getByText("Tormod - Haugland")
   })
 
-  it("allows for fetching multiple subtrees of the graph simultaneously", () => {
-    const dataGraph = graph({
-      user: {
-        firstName: "Tormod",
-        lastName: "Haugland" 
-      }
-    })
-
-    function Component(){
-      const [
-        [firstName],
-        [lastName]
-      ] = dataGraph.useGraph("user.firstName", "user.lastName")
-
-      return (
-        <div>
-          {firstName} - {lastName}
-        </div>
-      )
-    }
-
-    const { getByText } = render(
-      <dataGraph.Provider>
-        <Component />
-      </dataGraph.Provider>
-    )
-
-    getByText("Tormod - Haugland")
-  })
-
   it("updates the current context state when set is called", () => {
-    const dataGraph = graph({
+    const dataGraph = traph({
       user: {
         firstName: "Tormod",
         lastName: "Haugland" 
@@ -144,13 +126,16 @@ describe("graph", () => {
     getByText("Thomas")
     getByText("Thomas - Haugland")
   })
+})
+
+describe("advanced usages", () => {
 
   it("resolves nested graph context values, and enables updates", () => {
-    const dataGraph = graph({
-      user: graph({
+    const dataGraph = traph({
+      user: traph({
         firstName: "Tormod",
         lastName: "Haugland",
-        house: graph({
+        house: traph({
           address: "Storgata 40",
           postalCode: "0182",
           postalPlace: "Oslo"
@@ -181,8 +166,8 @@ describe("graph", () => {
   })
 
   it("allows for member functions of a graph to call updateGraph with new data", () => {
-    const dataGraph = graph({
-      user: graph({
+    const dataGraph = traph({
+      user: traph({
         firstName: "Tormod",
         lastName: "Haugland",
         likes: 5,
@@ -216,11 +201,11 @@ describe("graph", () => {
   })
 
   it("allows having different contexts with differing values for a graph", () => {
-    const subGraph = graph({
+    const subGraph = traph({
       subKey: "subInitialValue"
     })
 
-    const dataGraph = graph({
+    const dataGraph = traph({
       key: "initialValue",
       subGraph  
     })
@@ -238,7 +223,7 @@ describe("graph", () => {
     const { getByText } = render(
       <dataGraph.Provider>
         <Component />
-        <subGraph.Provider value={{subKey: "override"}}>
+        <subGraph.Provider graphData={{subKey: "override"}}>
           <Component />
         </subGraph.Provider>
       </dataGraph.Provider>
@@ -252,5 +237,373 @@ describe("graph", () => {
     // New node is different
     getByText("Updated subkey")
     
+  })
+
+  it("altering slices of the tree spanning multiple graphs updates the data correctly", () => {
+    const bottomLevel = traph({
+      deep: 0
+    })
+
+    const midLevel = traph({
+      middle: 10,
+      bottom: bottomLevel
+    })
+
+    const topLevel = traph({
+      top: 20,
+      middle: midLevel
+    })
+
+    function Component(){
+      const [graph, setGraph] = topLevel.useGraph()
+
+      return (
+        <div onClick={() => setGraph({
+          top: graph.top + 1,
+          middle: {
+            middle: graph.middle.middle + 1,
+            bottom: {
+              deep: graph.middle.bottom.deep + 1
+            }
+          }
+        })}>
+          {graph.top} - {graph.middle.middle} - {graph.middle.bottom.deep}
+        </div>
+      )
+    }
+
+    function ComponentUsingMidLevel(){
+      const [midGraph, setMidGraph] = midLevel.useGraph()
+      return (
+        <div onClick={(e) => {
+          setMidGraph({
+            middle: midGraph.middle + 1,
+            bottom: {
+              deep: midGraph.bottom.deep + 1
+            }
+          })
+          e.stopPropagation() 
+        }}>
+          {midGraph.middle} - {midGraph.bottom.deep}
+        </div>
+      )
+    }
+
+    function ComponentUsingBottomLevel(){
+      const [bottomGraph, setBottomGraph] = bottomLevel.useGraph()
+      return (
+        <div onClick={e => {
+          setBottomGraph({deep: bottomGraph.deep + 1})
+          e.stopPropagation()
+        }}>
+          {bottomGraph.deep}
+        </div>
+      )
+    }
+
+    const { getByText } = render(
+      <topLevel.Provider>
+        <Component />
+        <ComponentUsingBottomLevel />
+        <ComponentUsingMidLevel />
+      </topLevel.Provider>
+    )
+
+    const bottom = getByText("0")
+    const mid = getByText("10 - 0")
+    const top = getByText("20 - 10 - 0")
+
+    fireEvent.click(bottom)
+
+    getByText("1")
+    getByText("10 - 1")
+    getByText("20 - 10 - 1")
+
+    fireEvent.click(mid)
+
+    getByText("2")
+    getByText("11 - 2")
+    getByText("20 - 11 - 2")
+
+    fireEvent.click(top)
+
+    getByText("3")
+    getByText("12 - 3")
+    getByText("21 - 12 - 3")
+  })
+
+  it("deleting slices of the tree spanning multiple graphs updates the data correctly", () => {
+    // It should be possible to delete subgraphs from a top-level graph context
+    const Graph = traph({
+      top: "value",
+      subgraph: traph({
+        bottom: "value2"
+      })
+    })
+
+    function Component(){
+      const [graph, setGraph] = Graph.useGraph()
+
+      return (
+        <div onClick={() => setGraph({
+          subgraph: null
+        })}>
+          {graph.subgraph ? "Subgraph exists" : "Subgraph does not exist"} 
+        </div>
+      )
+    }
+
+    const { getByText } = render(
+      <Graph.Provider>
+        <Component />
+      </Graph.Provider>
+    )
+
+    const element = getByText("Subgraph exists")
+
+    fireEvent.click(element)
+
+    getByText("Subgraph does not exist")
+
+  })
+
+  it("subgraph mounted at multiple locations is updated at all locations", () => {
+
+    type Person = {
+      name: string,
+      age: number
+    }
+
+    const Mark = traph({
+      name: "Mark",
+      age: 24
+    })
+
+    const Susy = traph({
+      name: "Susy",
+      age: 25
+    })
+
+    const Mona = traph({
+      name: "Mona",
+      age: 55,
+      children: [
+        Mark,
+      ]
+    })
+
+    const Paul = traph({
+      name: "Paul",
+      age: 56,
+      children: [
+        Mark,
+        Susy
+      ]
+    })
+
+    const People = traph({
+      paul: Paul,
+      mona: Mona,
+      susy: Susy,
+      mark: Mark
+    })
+
+    function PaulComponent(){
+      // Alternatively Paul.useGraph()
+      const [paul, setPaul] = People.useGraph("paul")
+
+      return (
+        <div>
+          Paul is the father of: {paul.children.map((child: Person) => child.name).join(", ")}
+        </div>
+      )
+    }
+
+    function MonaComponent(){
+      // Alternatively Mona.useGraph()
+      const [mona, setMona] = People.useGraph("mona")
+
+      const context = useContext(Mark.Context)
+
+      return (
+        <div>
+          Mona is the mother of: {mona.children.map((child: Person) => child.name).join(", ")}
+        </div>
+      )
+    }
+
+    function MarkComponent(){
+      // Alternatively Mark.useGraph()
+      const [mark, setMark] = People.useGraph("mark")
+
+      return (
+        <div onClick={() => setMark({name: "John"})}>
+          Click me to change Mark's name!
+        </div>
+      )
+    }
+
+    const { getByText } = render(
+      <People.Provider>
+        <PaulComponent />
+        <MarkComponent />
+        <MonaComponent />
+      </People.Provider>
+    )
+
+    const markElement = getByText("Click me to change Mark's name!")
+
+    getByText("Paul is the father of: Mark, Susy")
+    getByText("Mona is the mother of: Mark")
+
+    fireEvent.click(markElement)
+
+    getByText("Paul is the father of: John, Susy")
+    getByText("Mona is the mother of: John")
+  })
+})
+
+describe('mergeGraphData', () => {
+  it('should accept the new value when given scalars', () => {
+    expect(mergeGraphData(1, 2)).toEqual(2)
+    expect(mergeGraphData("string", "otherString")).toEqual("otherString")
+    expect(mergeGraphData("string", null)).toEqual(null)
+    expect(mergeGraphData({}, null)).toEqual(null)
+    expect(mergeGraphData(undefined, null)).toEqual(null)
+    expect(mergeGraphData(null, undefined)).toEqual(undefined)
+
+  })
+  it('should recursively accept new values when given plain objects', () => {
+    expect(mergeGraphData({a: 2, c: 5}, {a: 3, b: 4})).toEqual({a: 3, b: 4, c: 5})
+  })
+  it('should recursively accept new values when given arrays', () => {
+    expect(mergeGraphData([2, 3, "string"], [2, 3, 4])).toEqual([2, 3, 4])
+  })
+  it('should take old values when given graph types', () => {
+    const graph = traph({
+      key: "value"
+    })
+    expect(mergeGraphData(graph, {sub: {key: "value"}})).toEqual(graph)
+  })
+  it('should recursively take new non-graph values, and old graph-values', () => {
+    const subGraph = traph({
+      key: "value"
+    })
+    const graph = {
+      a: 2,
+      b: subGraph,
+      c: {
+        d: 5,
+        e: 6
+      }
+    }
+    const newGraph = {
+      a: 9,
+      b: {
+        key: "value"
+      },
+      c: {
+        d: 10,
+      }
+    }
+
+    expect(mergeGraphData(graph, newGraph)).toEqual({
+      a: 9,
+      b: subGraph,
+      c: {
+        d: 10,
+        e: 6
+      }
+    })
+  })
+  it('should recursively take old values when there exists deep graphs', () => {
+    const subGraph = traph({
+      key: "value"
+    })
+    const graph = {
+      a: 2,
+      b: {
+        f: subGraph
+      },
+      c: {
+        d: 5,
+        e: 6
+      }
+    }
+    const newGraph = {
+      a: 9,
+      b: {
+        f: {
+          g: "some updated value deep down"
+        }
+      },
+      c: {
+        d: 10,
+      }
+    }
+    expect(mergeGraphData(graph, newGraph)).toEqual({
+      a: 9,
+      b: {
+        f: subGraph
+      },
+      c: {
+        d: 10,
+        e: 6
+      }
+    })
+  })
+  it('should recursively take new values when there exists deep arrays', () => {
+    // Here we also test deeply nested arrays
+    const graph = {
+      a: 2,
+      b: "string",
+      c: {
+        d: [2, 3, {a: 2}, 5]
+      }
+    }
+    const newGraph = {
+      a: 2,
+      b: "string",
+      c: {
+        d: [3, 3, {c: 3}, 6]
+      }
+    }
+    expect(mergeGraphData(graph, newGraph)).toEqual({
+      a: 2,
+      b: "string",
+      c: {
+        d: [3, 3, {a: 2, c: 3}, 6]
+      }
+    })
+  })
+  it('should recursively take old graphs when there exists deep arrays', () => {
+    // Here we also test deeply nested arrays
+    // Here we also test deeply nested arrays
+    const subGraph = traph({
+      a: 3
+    })
+
+    const graph = {
+      a: 2,
+      b: "string",
+      c: {
+        d: [2, 3, subGraph, 5]
+      }
+    }
+    const newGraph = {
+      a: 2,
+      b: "string",
+      c: {
+        d: [3, 3, {c: 3}, 6]
+      }
+    }
+
+    expect(mergeGraphData(graph, newGraph)).toEqual({
+      a: 2,
+      b: "string",
+      c: {
+        d: [3, 3, subGraph, 6]
+      }
+    })
   })
 })
